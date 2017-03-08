866608c69d0d418a86bff960f29be9e5ee17be27
Jenner Hanni <jenner@wickerbox.net>
2017-03-07 16:34:59 -0800

    MMA7660 Breakout board v1.1 tested. Release.

--- mma7660-sensor-breakout/README.md
+++ mma7660-sensor-breakout/README.md
@@ -1,20 +1,25 @@
 <!--- start title --->
 # MMA7660 3-axis Accelerometer Breakout v1.1
+
 A breadboard-compatible breakout board for the MMA7660 accelerometer.
 
-Updated: 31 Dec 2016
+Updated: 7 March 2017
 
-Author: Jenner Hanni
-Website: http://wickerbox.net
-Company: Wickerbox Electronics
-License: CERN Open Hardware License v1.2
+- Author: Jenner Hanni
+- Website: http://wickerbox.net
+- Company: Wickerbox Electronics
+- License: CERN Open Hardware License v1.2
 
 <!--- end title --->
 This is a breadboard-compatible breakout board for the MMA7660 accelerometer.
 
 The project is released as open hardware under the CERN v1.2 Open Hardware license.
 
-You can order a set of three bare boards for $1.25 <a href="https://oshpark.com/shared_projects/1vI6D3RK">from OSH Park</a>.
+The I2C lines have 1K pull-up resistors.
+
+You can order a set of three bare boards for $1.25 <a href="https://oshpark.com/shared_projects/YDCCxiCM">from OSH Park</a>.
+
+![Assembled](assembled.png)
 
 ![Schematic](schematic.png)
 
@@ -27,8 +32,6 @@ You can order a set of three bare boards for $1.25 <a href="https://oshpark.com/
 |J1|1|HEADER MALE 5POS TH 1x05 0.1”|952-1902-ND|
 |R1 R2|2|RES SMD 1K OHM 1% 1/10W 0603|1276-3484-1-ND|
 |U1|1|IC ACCELEROMETER 3X3 DGTL 10-DFN|MMA7660FCT-ND|
-
-
 <!--- bom end --->
 
 ![Gerber Preview](preview.png)
--- mma7660-sensor-breakout/code/mma7660-sensor-breakout-test/mma7660-sensor-breakout-test.ino
+++ mma7660-sensor-breakout/code/mma7660-sensor-breakout-test/mma7660-sensor-breakout-test.ino
@@ -41,23 +41,24 @@ void setup() {
   pinMode(ledPinGreen, OUTPUT);
 
   both_on();
-  delay(1000);
+  delay(200);
   green_on();
-  delay(1000);
+  delay(200);
   red_on();
-  delay(1000);
+  delay(200);
   both_off();
-  delay(1000);
+  delay(200);
    
+  Serial.println("3-axis Accelerometer MMA7660 Test");
+  delay(1000);
+
   if (accel.init() ==   1)
     green_on();
   else 
     red_on();
 
-  Serial.println("test");
-
   // wait a while
-  delay(5000); 
+  delay(2000); 
 }
 
 // the loop function runs over and over again forever
@@ -65,95 +66,38 @@ void loop() {
 
   accel.x = 0;
   accel.y = 0;
+  accel.z = 0;
   accel.getValues();  
 
   Serial.print(accel.x);
   Serial.print(" ");
-  Serial.println(accel.y);
+  Serial.print(accel.y);
+  Serial.print(" ");
+  Serial.println(accel.z);
+  delay(200);
       
-  if (accel.x > 50 || accel.y > 50) {
-    both_on();
-    delay(100);
-    green_on();
-    delay(100);
-    both_off();
-    delay(100);
-    red_on();
-  }
-  else if (accel.x < 5 || accel.y < 5) {
-    both_off();
-    delay(500);
-  }
-  else {
-    both_off();
-    delay(100);
-    green_on();
-    delay(500);
-    red_on();
-    delay(500);
-    both_on();
-  }
-  delay(1000);
+//  if (accel.x > 50 || accel.y > 50) {
+//    both_on();
+//    delay(100);
+//    green_on();
+//    delay(100);
+//    both_off();
+//    delay(100);
+//    red_on();
+//  }
+//  else if (accel.x < 5 || accel.y < 5) {
+//    both_off();
+//    delay(500);
+//  }
+//  else {
+//    both_off();
+//    delay(100);
+//    green_on();
+//    delay(500);
+//    red_on();
+//    delay(500);
+//    both_on();
+//  }
+//  delay(1000);
 
 }
-  // get accelerator data
-//  int temp = accel.x;
-//  if (temp > 0) {
-//    both_on();
-//    delay(10*accel.x);
-//    first_on();
-//    delay(10*accel.x);
-//    both_off();
-//    delay(10*accel.x);
-//    second_on();
-//  }
-//  else 
-//    both_on();
-
-  //Serial.print("X: ");Serial.println(accel.x, DEC);
-  //Serial.print("Y: ");Serial.println(accel.y, DEC);
-  //Serial.print("Z: ");Serial.println(accel.z, DEC);
-
-  // values in x, y, z seem to go 0-62 in DECimal
-
-  // Kingbright EG59EGC Common Cathode 3-pin Red/Yellow/Green
-  // Outer pins get pulled high to turn on, middle pin is always low.
-  // D2 = Pin 1 Green   D3 = Pin 3 Red
-  // Works fine
-
-  // Kingbright WP115WGYW Common Cathode 3-pin 
-  // Outer pins get pulled high to turn on, middle pin is always low.
-  // D2 = Pin 1 Green   D3 = Pin 3 Yellow 
-  // Very nice orange.
-
-  // SunLED XLUGR59M Common Cathode 3-pin
-  // Outer pins get pulled high to turn on, middle pin is always low.
-  // D2 = Pin 1 Green   D3 = Pin 3 Red 
-  // Very nice red and somewhat less nice orange than the other one
-  
-  // SunLED XLUGY34M Common Cathode 3-pin
-  // Outer pins get pulled high to turn on, middle pin is always low.
-  // D2 = Pin 1 Green   D3 = Pin 3 Yellow 
-  // Very nice orange, very nice yellow and green. Favorite of non-red ones.
-  
-//  digitalWrite(2, HIGH);   // turn the LED on (HIGH is the voltage level)
-//  delay(1000);              // wait for a second
-//  digitalWrite(2, LOW);    // turn the LED off by making the voltage LOW
-//  delay(1000);              // wait for a second
-//  digitalWrite(3, HIGH);   // turn the LED on (HIGH is the voltage level)
-//  delay(1000);              // wait for a second
-//  digitalWrite(3, LOW);    // turn the LED off by making the voltage LOW
-//  delay(1000);              // wait for a second
-//  digitalWrite(3, HIGH);   // turn the LED on (HIGH is the voltage level)
-//  delay(1000);              // wait for a second
-//  digitalWrite(2, HIGH);    // turn the LED off by making the voltage LOW
-//  delay(1000);              // wait for a second
-//  digitalWrite(3, LOW);   // turn the LED on (HIGH is the voltage level)
-//  delay(1000);              // wait for a second
-//  digitalWrite(2, LOW);    // turn the LED off by making the voltage LOW
-//  delay(1000);              // wait for a second
-
-  // Twinkle (Fluttery) Effect
-  // both on, 1 on, 3 on, both off
-//
-
